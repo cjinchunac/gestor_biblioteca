9bab5eb7598c01562500de45c1a6ef5b1bef66e3
felix <fmamanil@unjbg.edu.pe>
2025-07-15 13:33:35 -0500

    se agrega funcion modificar y borrar libros

--- libro.cpp
+++ libro.cpp
@@ -16,3 +16,36 @@ void imprimelibro(libro &lib) {
     cout << "Genero: " << lib.genero << endl;
     cout << "Disponibles: " << lib.cantidad << endl;
 }
+void modificardatolibro(libro &lib) {
+    cout << "Ingrese nuevos datos del libro:" << endl;
+    cout << "ID: "; cin >> lib.ID;
+    cin.ignore();
+    cout << "Nombre del libro: "; getline(cin, lib.titulo);
+    cout << "Autor: "; getline(cin, lib.autor);
+    cout << "Genero: "; getline(cin, lib.genero);
+    cout << "Cantidad disponible: "; cin >> lib.cantidad;
+    cout << "Libro modificado correctamente!" << endl;
+}
+void borrarlibro(libro lista_libros[], int &n) {
+    int ID, contador = 0;
+    cout << "Ingrese ID del libro a borrar: ";
+    cin >> ID;
+    
+    for (int i = 0; i < n; i++) {
+        if (lista_libros[i].ID == ID) {
+            cout << "\nDatos del libro a borrar:" << endl;
+            imprimelibro(lista_libros[i]);
+            
+            for (int j = i; j < n-1; j++) {
+                lista_libros[j] = lista_libros[j+1];
+            }
+            contador++;
+            n--;
+            cout << "Libro eliminado correctamente!" << endl;
+            break;
+        }
+    }
+    
+    if (contador == 0)
+        cout << "No se encontro libro con esa ID" << endl;
+}
