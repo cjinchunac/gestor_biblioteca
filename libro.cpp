7b4fcdc901e80546bacbffbeea3fbfe154e8a6f0
felix <fmamanil@unjbg.edu.pe>
2025-07-15 13:36:33 -0500

    desarrollo de la funcion devolver libro

--- libro.cpp
+++ libro.cpp
@@ -49,3 +49,41 @@ void borrarlibro(libro lista_libros[], int &n) {
     if (contador == 0)
         cout << "No se encontro libro con esa ID" << endl;
 }
+void devolverLibro(libro lista_libros[], int n_libros, prestamo prestamos[], int &n_prestamos) {
+    if (n_prestamos == 0) {
+        cout << "No hay prestamos registrados para devolver." << endl;
+        return;
+    }
+    
+    int DNI, ID;
+    cout << "Ingrese DNI del usuario: ";
+    cin >> DNI;
+    cout << "Ingrese ID del libro a devolver: ";
+    cin >> ID;
+    
+    int encontrado = 0;
+    
+    for (int i = 0; i < n_prestamos; i++) {
+        if (prestamos[i].usuarioPrestamo.DNI == DNI && 
+            prestamos[i].libroPrestado.ID == ID && 
+            prestamos[i].devuelto == 0) {
+            
+            prestamos[i].devuelto = 1;
+            
+            for (int j = 0; j < n_libros; j++) {
+                if (lista_libros[j].ID == ID) {
+                    lista_libros[j].cantidad++;
+                    break;
+                }
+            }
+            
+            cout << "Libro devuelto con exito!" << endl;
+            encontrado = 1;
+            break;
+        }
+    }
+    
+    if (!encontrado) {
+        cout << "No se encontro un prestamo activo con esos datos." << endl;
+    }
+}
