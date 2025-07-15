d9442cac0d2562a9881402205b7eb6b6380aa4d7
felix <fmamanil@unjbg.edu.pe>
2025-07-15 13:31:27 -0500

    desarrollo de la funcion imprimir libro

--- libro.cpp
+++ libro.cpp
@@ -9,4 +9,10 @@ void leerlibro(libro &lib, int id, string tit, string aut, string gen, int cant)
     lib.genero = gen;
     lib.cantidad = cant;
 }
-
+void imprimelibro(libro &lib) {
+    cout << "ID: " << lib.ID << endl;
+    cout << "Titulo: " << lib.titulo << endl;
+    cout << "Autor: " << lib.autor << endl;
+    cout << "Genero: " << lib.genero << endl;
+    cout << "Disponibles: " << lib.cantidad << endl;
+}
