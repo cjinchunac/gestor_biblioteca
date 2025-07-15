26e083b6be3b01246456bdca7f8fc25ac383947f
felix <fmamanil@unjbg.edu.pe>
2025-07-15 13:30:20 -0500

    desarrollo de la funcion leer libro

--- libro.cpp
+++ libro.cpp
@@ -2,3 +2,11 @@
 #include<iostream>
 #include<string>
 using namespace std;
+void leerlibro(libro &lib, int id, string tit, string aut, string gen, int cant) {
+    lib.ID = id;
+    lib.titulo = tit;
+    lib.autor = aut;
+    lib.genero = gen;
+    lib.cantidad = cant;
+}
+
